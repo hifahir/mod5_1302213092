d37727e84f5462f6cfe5b632b13c3bed9a99b4b9
hifahir <putratertuahirzi@gmail.com>
2023-03-21 16:53:14 +0700

    menambahkan class SimpleDataBase

--- mod5_1302213092/mod5_1302213092/Program.cs
+++ mod5_1302213092/mod5_1302213092/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDate;
+
+    public SimpleDataBase()
+    {
+        this.storedData= new List<T>();
+        this.inputDate= new List<DateTime>();
+    }
+
+    public void addNewData(T newData)
+    {
+        this.inputDate.Add(DateTime.Now);
+        this.storedData.Add(newData);
+
+    }
+    public void PrintAllData()
+    {
+        for(int i = 0; i < this.inputDate.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " bersisi " + this.storedData[i] + " yang disimpan pada waktu UTC: " + this.inputDate[i]);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        SimpleDataBase<float> dataBase = new SimpleDataBase<float>();
+        dataBase.addNewData(13);
+        dataBase.addNewData(20);
+        dataBase.PrintAllData();
+
+
+    }
+}
